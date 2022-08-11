cf464a24097ac48c66d73d059b03ac8e98285701
SamehAwad <67038060+SamehAwad@users.noreply.github.com>
2022-08-11 04:31:42 +0200

    create category controller

---
+++ controllers/categoryController.js
@@ -0,0 +1,9 @@
+const Category = require('../models/category');
+const factory = require('./handlerFactory');
+// const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllCategories = factory.getAll(Category);
+exports.getCategory = factory.getOne(Category);
+exports.createCategory = factory.createOne(Category);
+exports.updateCategory = factory.updateOne(Category);
+exports.deleteCategory = factory.deleteOne(Category);
